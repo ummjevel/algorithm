02b4bcb881f86d22dcb782fecbef6b2f53b26f52
simyeen <simyeen@naver.com>
2020-09-06 22:29:58 +0900

    11053.py

---
+++ nam12/dp/11053.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+
+d = [0 for i in range(n)]
+
+for i in range(n):  # 0부터 끝까지
+    for j in range(i):  # i까지
+        if a[i] > a[j] and d[i] < d[j]:
+            d[i] = d[j]
+    d[i] += 1
+
+print(max(d))
+
+# O(n^2)
+# 이 방법이 최선인가 ?? stack써서 index저장하듯이하면...?
