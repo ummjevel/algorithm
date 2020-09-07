66bb0392e6e5eaba17e4b82fbb008eb4a9de99ef
simyeen <simyeen@naver.com>
2020-09-07 19:02:38 +0900

    1699.py time out

--- nam12/dp/1699.py
+++ nam12/dp/1699.py
@@ -1,10 +1,32 @@
-n = int(input())
+# n = int(input())
+
+# d = [0 for _ in range(n+1)]
 
 
-cnt = 0
-while n:
+# d[1] = 1
 
-    n = n - (int(n ** 0.5))**2
-    cnt += 1
-    print(n)
-print(cnt)
+# for i in range(2, n+1):
+
+#     if i**2 < n:
+#         d[i**2] = 1
+#     min = 999999
+#     for j in range(0, i//2):
+#         if min > d[j] + d[i-j]:
+#             min = d[j] + d[i-j]
+#     d[i] = min
+
+#     print(d[i])
+
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+d = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    d[i] = i
+    for j in range(1, i):
+        if j**2 > i:
+            break
+        elif d[i] > d[i - j**2] + 1:
+            d[i] = d[i - j**2] + 1
+print(d[n])
