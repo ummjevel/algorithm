adb1dbd0ada8dcbd9f5b8fb2b8121d4908e3df64
simyeen <simyeen@naver.com>
2020-09-27 18:39:42 +0900

    11055.py

--- nam12/dp/11055.py
+++ nam12/dp/11055.py
@@ -3,4 +3,14 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 a = list(map(int, sys.stdin.readline().rstrip().split()))
 
-d = [0 for i in range(n)]
+d = [0 for i in range(n+5)]
+
+for i in range(n):
+    temp = 0
+    for j in range(i):
+        if temp < a[j] and a[j] < a[i]:
+            d[i] += a[j]
+            temp = a[j]
+    d[i] += a[i]
+
+print(max(d))
