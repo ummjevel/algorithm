0475e0108fce76262b46bc13fb24934eb6a68c92
simyeen <simyeen@naver.com>
2020-09-28 02:00:26 +0900

    RGB distance

--- nam12/dp/17404.py
+++ nam12/dp/17404.py
@@ -4,15 +4,15 @@ n = int(sys.stdin.readline().rstrip())
 
 p = [[0]*3 for _ in range(n+1)]
 d = [[0]*3 for _ in range(n+1)]
+path = [[0]*3 for _ in range(n+1)]
 
 for i in range(1, n+1):
     p[i] = list(map(int, (sys.stdin.readline().rstrip().split())))
 
-d[1] = p[1]
+# d[1][0].append(p[1][0])
+# d[1][1].append(p[1][1])
+# d[1][2].append(p[1][2])
 
-
-for i in range(2, n+1):
-    d[i][0] = p[i][0] + min(d[i-1][1], d[i-1][2])
-    d[i][1] = p[i][1] + min(d[i-1][2], d[i-1][0])
-    d[i][2] = p[i][2] + min(d[i-1][0], d[i-1][1])
-print(min(d[n]))
+print(p, d)
+print(p[1][0])
+d[1][0]
