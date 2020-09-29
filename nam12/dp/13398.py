1659e20778e3de0384e2725633d3b12c0912ce5f
simyeen <simyeen@naver.com>
2020-09-29 18:00:08 +0900

    13398.py

--- nam12/dp/13398.py
+++ nam12/dp/13398.py
@@ -1,16 +1,41 @@
+# import sys
+
+# n = int(sys.stdin.readline().rstrip())
+# a = list(map(int, sys.stdin.readline().rstrip().split()))
+# d = [[0]*2 for _ in range(n+10)]
+
+# d[1][1] = a[1]
+# d[1][0] = 0
+
+# for i in range(1, n):
+#     d[i][1] = a[i]
+#     d[i][0] = max(d[i-1])
+
+#     d[i] = max(d[i], d[i-1] + a[i])
+
+# print(max(d))
+
 import sys
 
 n = int(sys.stdin.readline().rstrip())
 a = list(map(int, sys.stdin.readline().rstrip().split()))
-d = [[0]*2 for _ in range(n+10)]
 
-d[1][1] = a[1]
-d[1][0] = 0
+incre = [0]*n
+decre = [0]*n
+d = [0]*n
+
+incre[0] = a[0]
+decre[n-1] = a[n-1]
 
 for i in range(1, n):
-    d[i][1] = a[i]
-    d[i][0] = max(d[i-1])
+    incre[i] = max(incre[i-1]+a[i], a[i])
 
-    d[i] = max(d[i], d[i-1] + a[i])
+for i in range(n-2, -1, -1):
+    decre[i] = max(decre[i+1]+a[i], a[i])
 
-print(max(d))
+d[1] = a[1]
+d[n-1] = a[n-1]
+for i in range(1, n-1):
+    d[i] = incre[i-1] + decre[i+1]
+
+print(max(max(incre), max(d)))
