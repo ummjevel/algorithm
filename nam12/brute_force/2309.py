e04a9df05f7173b3b3f09d6bf0f1de2f57cf9964
simyeen <simyeen@naver.com>
2020-10-02 02:27:35 +0900

    2309.py clear

--- nam12/brute_force/2309.py
+++ nam12/brute_force/2309.py
@@ -22,3 +22,21 @@
 #                     continue
 #                 else:
 #                     print(char)
+
+dwf = []
+for i in range(9):
+    dwf.append(int(input()))
+
+dwf = sorted(dwf)
+dwf_s = sum(dwf)
+
+for i in range(9):
+    for j in range(i+1, 9):
+        if dwf_s - (dwf[i] + dwf[j]) == 100:
+            for k in range(9):
+                if k == i or k == j:
+                    continue
+                else:
+                    print(dwf[k])
+            exit()
+# 마지막에 break말고 exit()로 안 걸어주면 틀렸다고 나옴 뭐지...
