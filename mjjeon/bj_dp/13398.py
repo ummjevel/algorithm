640044f47e426b8620e10fbcc6169d140593a168
MinJeong Jeon <mseagle2023@gmail.com>
2020-10-04 16:25:38 +0900

    last dp1

--- mjjeon/bj_dp/13398.py
+++ mjjeon/bj_dp/13398.py
@@ -13,13 +13,15 @@ for i in range(n):
     ans = max(ans, max(D[i]))
 print(ans)
 
+# 그 전 것보다 큰 것으로 찾도록, ans 라는 변수를 두고 돌려가면서 확인..
 
-# 틀렸습니다. 왜?
+
+# 틀렸습니다. : 전체 다 더한 것, 하나만 빼고 더한 것으로 두 개로 나눠서 진행했다
 # n = int(input())
 # N = list(map(int, input().split()))
 
 # D = [[0, 0] for i in range(n)]
-# D[0] = [N[0], -1000000]
+# D[0] = [N[0], -999999999]
 # for i in range(1, n):
 #     D[i][0] = max(N[i], D[i-1][0] + N[i])          # 삭제 x
 #     D[i][1] = max(D[i-1][0], D[i-1][1] + N[i])     # 삭제 o
@@ -27,7 +29,7 @@ print(ans)
 # print(max(max(D)))
 
 
-# 틀렸습니다.
+# 틀렸습니다. : 처음에는 작은 수를 찾아서 빼줬었다..
 
 # n = int(input())
 # N = list(map(int, input().split()))
--- mjjeon/bj_dp/17404.py
+++ mjjeon/bj_dp/17404.py
@@ -3,23 +3,6 @@ N = int(sys.stdin.readline())
 D = [[0,0,0] for i in range(N)]
 result = 100000
 input_list = []
-# for i in range(1, N):
-# # for i in range(N - 1):
-#     house = list(map(int, sys.stdin.readline().split()))
-#     D[i][0] = min(D[i-1][1], D[i-1][2]) + house[0]
-#     D[i][1] = min(D[i-1][0], D[i-1][2]) + house[1]
-#     D[i][2] = min(D[i-1][0], D[i-1][1]) + house[2]
-
-# house = list(map(int, sys.stdin.readline().split()))
-# D[N-1][0] = min(D[N-2][1], D[N-2][2], D[0][1], D[0][2]) + house[0]
-# D[N-1][1] = min(D[N-2][0], D[N-2][2], D[0][0], D[0][2]) + house[1]
-# D[N-1][2] = min(D[N-2][0], D[N-2][1], D[0][0], D[0][1]) + house[2]
-
-# house = list(map(int, sys.stdin.readline().split()))
-# D[0][0] = min(D[-1][1], D[-1][2]) + house[0]
-# D[0][1] = min(D[-1][0], D[-1][2]) + house[1]
-# D[0][2] = min(D[-1][0], D[-1][1]) + house[2]
-
 
 for i in range(N):
     input_list.append(list(map(int, sys.stdin.readline().split())))
@@ -33,21 +16,28 @@ for index in range(3):
         D[i][1] = min(D[i-1][0], D[i-1][2]) + input_list[i][1]
         D[i][2] = min(D[i-1][0], D[i-1][1]) + input_list[i][2]
 
-    if index == 0:
-        D[N-1][0] = 100000
-        D[N-1][1] = min(D[N-1][0], D[N-1][2]) + input_list[N-1][1]
-        D[N-1][2] = min(D[N-1][0], D[N-1][1]) + input_list[N-1][2]
-    elif index == 1:
-        D[N-1][0] = min(D[N-1][1], D[N-1][2]) + input_list[N-1][0]
-        D[N-1][1] = 100000
-        D[N-1][2] = min(D[N-1][0], D[N-1][1]) + input_list[N-1][2]
-    elif index == 2:
-        D[N-1][0] = min(D[N-1][1], D[N-1][2]) + input_list[N-1][0]
-        D[N-1][1] = min(D[N-1][0], D[N-1][2]) + input_list[N-1][1]
-        D[N-1][2] = 100000
+    for i in range(3):
+        if i == index:
+            continue
+        result = min(result, D[N-1][i])
 
-    result = min(result, min(D[N-1]))
-    print(D)
+    # 틀렸습니다. -> 마지막 부분을 변수값에 저장하도록.. index에 해당하는 것은 안하고.
+    # if index == 0:
+    #     D[N-1][0] = 100000
+    #     D[N-1][1] = min(D[N-1][0], D[N-1][2]) + input_list[N-1][1]
+    #     D[N-1][2] = min(D[N-1][0], D[N-1][1]) + input_list[N-1][2]
+    # elif index == 1:
+    #     D[N-1][0] = min(D[N-1][1], D[N-1][2]) + input_list[N-1][0]
+    #     D[N-1][1] = 100000
+    #     D[N-1][2] = min(D[N-1][0], D[N-1][1]) + input_list[N-1][2]
+    # elif index == 2:
+    #     D[N-1][0] = min(D[N-1][1], D[N-1][2]) + input_list[N-1][0]
+    #     D[N-1][1] = min(D[N-1][0], D[N-1][2]) + input_list[N-1][1]
+    #     D[N-1][2] = 100000
+
+    #print(D)
 
 
 print(result)
+
+# https://copy-driven-dev.tistory.com/m/78
