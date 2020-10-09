cb0986d16c1af7cf15e0723727a72fdbeab7aff8
simyeen <simyeen@naver.com>
2020-10-09 22:23:39 +0900

    1107.py

--- nam12/brute_force/1107.py
+++ nam12/brute_force/1107.py
@@ -7,13 +7,14 @@
 # 그리고 +, - 로 접근하기
 # 2. +,- 로만 접근한 것이 채널이동 보다 작은지 확인해주기.
 
-from itertools import permutations
+from itertools import product
 
 cur = 100  # 시작 채널.
 cnt = [0, 0]
 
 goal = input()
 leng = len(goal)
+goal = int(goal)
 
 n = int(input())
 remove_list = list(map(int, input().split()))
@@ -21,19 +22,22 @@ remocon_list = [i for i in range(10)]
 for i in range(n):
     remocon_list.remove(remove_list[i])
 
-cnt[0] = abs(int(goal) - cur)  # +, -로만 이동하기.
+cnt[0] = abs(goal - cur)  # +, -로만 이동하기.
 
 plag = 600000
-for c in permutations(remocon_list, leng):  # 순열함수 쓰면 tuple로 나오는데 변환시키는게 지랄같음.
+for c in product(remocon_list, repeat=leng):  # 중복 순열 사용.
     temp = "".join(map(str, c))
 
-    if cnt[0] < int(temp):
+    if cnt[0] < abs(goal - int(temp)):  # 번호 입력한게 더 멀때.
         continue
 
-    if abs(int(goal) - int(temp)) < plag:
-        plag = abs(int(goal) - int(temp))
+    if abs(goal - int(temp)) < plag:
+        plag = abs(goal - int(temp))
+        cur = temp
 
-if cnt[0] < cnt[1]:
+cnt[1] = leng + abs(goal-int(cur))
+
+if cnt[0] > cnt[1]:
     print(cnt[1])
     exit()
 else:
