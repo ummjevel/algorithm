9382a68eeacd16698e25d6e230086e82117e2cdc
simyeen <simyeen@naver.com>
2020-10-13 21:44:11 +0900

    15649(backtracking)

--- nam12/brute_force/15649.py
+++ nam12/brute_force/15649.py
@@ -1,9 +1,34 @@
-from itertools import permutations
+# from itertools import permutations
+
+# n, m = map(int, input().split())
+# arr = []
+# for i in range(1, n+1):
+#     arr.append(i)
+
+# for c in permutations(arr, m):
+#     print(" ".join(map(str, c)))
 
 n, m = map(int, input().split())
+check = [False] * n
+num = [i for i in range(1, n+1)]
 arr = []
-for i in range(1, n+1):
-    arr.append(i)
 
-for c in permutations(arr, m):
-    print(" ".join(map(str, c)))
+
+def dfs(idx):
+    if(idx == m):
+        print(*arr)
+        return
+
+    for i in range(n):
+        if(check[i]):
+            continue
+
+        check[i] = True
+        arr.append(num[i])
+        dfs(idx+1)
+        arr.pop()
+        print(arr)
+        check[i] = False
+
+
+dfs(0)
--- nam12/brute_force/15650.py
+++ nam12/brute_force/15650.py
@@ -1,18 +1,22 @@
-from itertools import permutations
+# from itertools import permutations
 
-n, m = map(int, input().split())
-arr = []
-for i in range(1, n+1):
-    arr.append(i)
+# n, m = map(int, input().split())
+# arr = []
+# for i in range(1, n+1):
+#     arr.append(i)
 
-check = False
-for c in permutations(arr, m):
-    check = False
-    for i in range(len(c)-1):
-        if int(c[i]) < int(c[i+1]):
-            check = True
-        else:
-            check = False
-            break
-    if(check):
-        print(" ".join(map(str, c)))
+# check = False
+# for c in permutations(arr, m):
+#     check = False
+#     for i in range(len(c)-1):
+#         if int(c[i]) < int(c[i+1]):
+#             check = True
+#         else:
+#             check = False
+#             break
+#     if(check):
+#         print(" ".join(map(str, c)))
+
+# 순열이랑 조합쓰면 좋을 줄 알았는데 index 구하기도 힘들고,
+# 무조건 tuple로 반환되는거라 원소마다 형변환 비교해줘서 별로인듯.
+# case가 클 땐 재귀로 돌리는 brute force가 제일 나은거 같다.(back tracking)
