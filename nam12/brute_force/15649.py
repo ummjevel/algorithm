68479d32c35235bae272dff5b70d7c566594dd02
simyeen <simyeen@naver.com>
2020-12-23 20:08:22 +0900

    restart algolrithm

--- nam12/brute_force/15649.py
+++ nam12/brute_force/15649.py
@@ -14,20 +14,18 @@ num = [i for i in range(1, n+1)]
 arr = []
 
 
-def dfs(idx):
-    if(idx == m):
+def dfs(cntM):
+    if(cntM == m):
         print(*arr)
         return
 
     for i in range(n):
         if(check[i]):
             continue
-
         check[i] = True
         arr.append(num[i])
-        dfs(idx+1)
+        dfs(cntM+1)
         arr.pop()
-        print(arr)
         check[i] = False
 
 
