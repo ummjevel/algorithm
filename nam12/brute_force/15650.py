2717798965a4dc6a509c8bfbb0b3f997eead4fcf
simyeen <simyeen@naver.com>
2020-12-23 20:25:13 +0900

    using conbination library

--- nam12/brute_force/15650.py
+++ nam12/brute_force/15650.py
@@ -20,33 +20,36 @@
 # 순열이랑 조합쓰면 좋을 줄 알았는데 index 구하기도 힘들고,
 # 무조건 tuple로 반환되는거라 원소마다 형변환 비교해줘서 별로인듯.
 # case가 클 땐 재귀로 돌리는 brute force가 제일 나은거 같다.(back tracking)
+# N, M = map(int, input().split())
+
+# num_list = [i + 1 for i in range(N)]
+# check_list = [False] * N
+# arr = []
+
+
+# def dfs(cnt):
+#     if(cnt == M):
+#         print(*arr)
+#         return
+
+#     for i in range(0, N):
+#         if(check_list[i]):
+#             continue
+
+#         check_list[i] = True
+#         arr.append(num_list[i])
+#         dfs(cnt + 1)
+#         arr.pop()
+
+#         for j in range(i + 1, N):
+#             check_list[j] = False
+#         # check_list[i] = False하면은 전부다 F가 되버려서 안됨.
+# dfs(0)
+
+from itertools import combinations
+
 N, M = map(int, input().split())
 
-num_list = [i + 1 for i in range(N)]
-check_list = [False] * N
-
-arr = []
-
-
-def dfs(cnt):
-    if(cnt == M):
-        print(*arr)
-        return
-
-    for i in range(0, N):
-        if(check_list[i]):
-            continue
-
-        check_list[i] = True
-        arr.append(num_list[i])
-        dfs(cnt + 1)
-        arr.pop()
-
-        # 이 부분이 순열하고의 차이점이다.
-        # 큰 나무에서 전에 봤던 것만
-        # 닫아주면 된다.
-        for j in range(i + 1, N):
-            check_list[j] = False
-
-
-dfs(0)
+c = combinations(range(1, N+1), M)
+for i in c:
+    print(' '.join(map(str, i)))
