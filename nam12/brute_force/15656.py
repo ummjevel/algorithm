85285e8ebab33f94b558502c7fb94cfcf788eba0
simyeen <simyeen@naver.com>
2020-12-23 21:26:16 +0900

    15656.py

---
+++ nam12/brute_force/15656.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+
+check = [False for i in range(N)]
+ans = []
+
+
+def dfs(cnt):
+
+    if cnt == M:
+        print(*ans)
+        return
+
+    for i in range(N):
+
+        if (check[i]):
+            continue
+
+        ans.append(num[i])
+        dfs(cnt+1)
+        check[i] = True
+        ans.pop()
+        for j in range(i+1, N):
+            check[j] = False
+
+
+dfs(0)
